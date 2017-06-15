a1bc7521cf5e4d9cfdf2b5c1a0988a4980922fe8
maxeskell <maxeskell1@gmail.com>
2017-06-15 15:54:59 +0100

    possible error with oauth github

--- db/seeds.js
+++ db/seeds.js
@@ -9,36 +9,36 @@ const User = require('../models/User');
 User.collection.drop();
 WildlifePost.collection.drop();
 
-// User
-// .create([{
-//   username: 'maxeskell',
-//   email: 'max@hotmail.com',
-//   postcode: 'SW1 1HW',
-//   lat: 51.515,
-//   lng: -0.05,
-//   image: '0a162dd6-afb3-4d0e-9458-b44edfc36712.jpeg',
-//   password: '',
-//   githubId: 12345678,
-//   facebookId: 123456
-// }])
-// .then((users) => {
-//   console.log(`${users.length} user(s) created!`);
-//   return WildlifePost
-//   .create([{
-//     title: 'Otter',
-//     createdBy: users[0],
-//     mainContent: 'otter',
-//     description: 'otter in river',
-//     image: '0a162dd6-afb3-4d0e-9458-b44edfc36712.jpeg',
-//     lat: 51.5103,
-//     lng: -0.07,
-//     keywords: ['otter', 'fauna'],
-//     comments: [{
-//       content: 'content',
-//       createdBy: users[0]
-//     }]
-//   }]);
-// })
+User
+.create([{
+  username: 'markescort',
+  email: 'mark@escort.com',
+  postcode: 'SW1 1HW',
+  lat: 51.515,
+  lng: -0.05,
+  image: 'download.jpeg',
+  password: '',
+  githubId: '',
+  facebookId: ''
+}])
+.then((users) => {
+  console.log(`${users.length} user(s) created!`);
+  return WildlifePost
+  .create([{
+    title: 'Owl on the wing',
+    createdBy: users[0],
+    mainContent: 'Barn owl',
+    description: 'Close up of white owl in flight over a green field',
+    image: 'download.jpeg',
+    lat: 51.5103,
+    lng: -0.07,
+    keywords: ['owl', 'bird'],
+    comments: [{
+      content: 'Great photo!',
+      createdBy: users[0]
+    }]
+  }]);
+})
 .then((wildlifePosts) => {
   console.log(`${wildlifePosts.length} WildlifePost(s) created!`);
 })
