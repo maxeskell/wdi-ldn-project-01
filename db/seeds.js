282bb06bd78ad86978a982833a675abdfd64e8a5
maxeskell <maxeskell1@gmail.com>
2017-06-15 16:10:10 +0100

    possible error with oauth github

--- db/seeds.js
+++ db/seeds.js
@@ -3,8 +3,8 @@ mongoose.Promise = require('bluebird');
 const { dbURI } = require('../config/environment');
 mongoose.connect(dbURI);
 
-const WildlifePost = require('./models/WildlifePost');
-const User = require('./models/User');
+const WildlifePost = require('../models/wildlifePost');
+const User = require('../models/user');
 
 User.collection.drop();
 WildlifePost.collection.drop();
